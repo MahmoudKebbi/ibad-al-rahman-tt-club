e8912060cffe865c0bc357fa76900767a33c141d
MahmoudKebbi <mahmoud.kebbi180@gmail.com>
2025-07-08 21:55:59 +0300

    Sampl config file for Firebase configuration

---
+++ src/services/firebase/config.example.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "localhost:3000",
+  projectId: "PROJECT-ID",
+  storageBucket: "PROJECT-ID.appspot.com",
+  messagingSenderId: "SENDER-ID",
+  appId: "APP-ID",
+  measurementId: "MEASUREMENT-ID",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
